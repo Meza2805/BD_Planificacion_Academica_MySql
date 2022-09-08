cc450de70cccba8e0d2f1c0c564da0f7be4704ba
Meza2805 <101306102+Meza2805@users.noreply.github.com>
2022-09-08 13:19:16 -0600

    Primer procedimiento almacenado

---
+++ Scripts/Procedimientos Almacenados/Insert_Personal.sql
@@ -0,0 +1,35 @@
+delimiter //
+create procedure SP_Insertar_Personal
+(
+	_Cedula_Personal char(18),
+	_Primer_Nombre varchar(35),
+	_Segundo_Nombre varchar(35),
+	_Primer_Apellido varchar(35),
+	_Segundo_Apellido varchar(35),
+    _Fecha_Nacimiento date,
+	_Sexo char(1),
+	_Telefono char(8),
+    _Direccion varchar(100),
+    _cargo int
+)
+begin
+	/*INICIALIZANDO ALGUNAS VARIABLES*/
+		DECLARE _Usser varchar(20);
+		 DECLARE _Contra varchar(20);
+		 DECLARE _Horas int;
+         declare _estado bit;
+         set _Horas = 10;
+         set _Usser = lower(CONCAT(_Primer_Nombre , "123"));
+         set _Contra = lower(CONCAT(_Primer_Nombre , "123"));
+         set _estado = 0;
+	if (not exists (select CEDULA from PERSONAL where STRCMP(CEDULA,_Cedula_Personal)  = 0) ) then
+			insert into PERSONAL(CEDULA,PRIMER_NOMBRE,SEGUNDO_NOMBRE,PRIMER_APELLIDO,SEGUNDO_APELLIDO,FECHA_NACIMIENTO,SEXO,TELEFONO,DIRECCION,HORAS_LABORALES,USSER,CONTRA,ESTADO,ID_CARGO,FECHA_REGISTRO) 
+			values (upper(_Cedula_Personal),upper(_Primer_Nombre),upper(_Segundo_Nombre),upper(_Primer_Apellido),upper(_Segundo_Apellido),_Fecha_Nacimiento,upper(_Sexo),_Telefono,upper(_Direccion),_Horas,_Usser,_Contra,_estado,_cargo,curdate());
+	
+    select 'EL PERSONAL HA SIDO REGISTRADO EN LA BASE DE DATOS' AS 'MENSAJE';
+    else
+			select 'EL PERSONAL YA EXISTE EN LA BASE DE DATOS' AS 'MENSAJE';
+	end if; 
+  
+end//
+delimiter ;
